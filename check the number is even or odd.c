72a0cbe9ef121b0ac3f5d2dea66cc9d9add67e60
Snigdha Singh <snigdhasingh3443@gmail.com>
2025-09-03 15:58:45 +0530

    Add files via upload

---
+++ check the number is even or odd.c
@@ -0,0 +1,18 @@
+//Program to check whether given number is even or odd
+//Developed by Snigdha Date 3/9/2025
+#include <stdio.h>
+void main ()
+{
+    int num;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    if(num % 2 ==0);
+    {
+        printf("%d is Even.\n", num);
+    }
+    elseif :
+    {
+    printf("%d is odd.\n", num);
+    }
+    retrun 0;
+}
