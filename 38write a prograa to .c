b03a094fabd106e41056c145bf3985c89aac8cb8
Snigdha Singh <snigdhasingh3443@gmail.com>
2025-10-29 15:55:01 +0530

    Add files via upload

---
+++ 38write a prograa to .c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10], key, found = 0;
+
+    printf("Enter 10 integers:\n");
+    for(int i = 0; i < 10; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter key to search: ");
+    scanf("%d", &key);
+
+    for(int i = 0; i < 10; i++) {
+        if(arr[i] == key) {
+            printf("Key found at position %d\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if(!found)
+        printf("Key not found.\n");
+
+    return 0;
+}
