1c4daf0bcaf83968ebc979b02a5f77c8798bb562
Snigdha Singh <snigdhasingh3443@gmail.com>
2025-10-29 16:12:09 +0530

    Add files via upload

---
+++ 39, write a program to read and print 2D array.c
@@ -0,0 +1,24 @@
+// program to read and pe\rint 2D  array
+// developed by snigdha
+//date 29/10/25
+#include <stdio.h>
+
+int main() {
+    int arr[3][3];
+    printf("Enter elements of 3x3 matrix:\n");
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("\nMatrix is:\n");
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ 40 program to print addition of 2 matrices3x3.c
@@ -0,0 +1,28 @@
+//program to print addition of 2 matrices 3x3
+//developed by snigdha
+//date 29/10/25
+#include <stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], sum[3][3];
+    printf("Enter first 3x3 matrix:\n");
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second 3x3 matrix:\n");
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++)
+            scanf("%d", &b[i][j]);
+
+    printf("\nSum of matrices:\n");
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
