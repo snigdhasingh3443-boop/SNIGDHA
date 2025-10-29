7484639dc8e15e431cf58ef364ad22e69b36e06f
Snigdha Singh <snigdhasingh3443@gmail.com>
2025-10-29 16:16:22 +0530

    Update 39, write a program to read and print 2D array

--- 39, write a program to read and print 2D array.c
+++ 39, write a program to read and print 2D array.c
@@ -1,24 +1,30 @@
-// program to read and pe\rint 2D  array
+// program to read and print 2D  array
 // developed by snigdha
 //date 29/10/25
 #include <stdio.h>
 
-int main() {
+void main()
+{
     int arr[3][3];
     printf("Enter elements of 3x3 matrix:\n");
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
+    for(int i = 0; i < 3; i++)
+   {
+        for(int j = 0; j < 3; j++)
+        {
             scanf("%d", &arr[i][j]);
         }
     }
 
     printf("\nMatrix is:\n");
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
+    for(int i = 0; i < 3; i++)
+        {
+        for(int j = 0; j < 3; j++)
+            {
             printf("%d ", arr[i][j]);
-        }
+           }
         printf("\n");
     }
 
-    return 0;
+
 }
+
