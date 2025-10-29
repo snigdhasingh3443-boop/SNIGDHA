03bb35c3f1e77cedab4072332afd91e97fed683b
Snigdha Singh <snigdhasingh3443@gmail.com>
2025-10-29 16:25:30 +0530

    Update 377. write a program to take 10 value from user and print even number.c

--- 377. write a program to take 10 value from user and print even number.c
+++ 377. write a program to take 10 value from user and print even number.c
@@ -3,17 +3,21 @@
 // date 29/10/25
 #include <stdio.h>
 
-int main() {
+void main()
+{
     int arr[10];
     printf("Enter 10 integers:\n");
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10; i++)
+        {
         scanf("%d", &arr[i]);
-    }
+       }
 
     printf("\nEven numbers are:\n");
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10; i++)
+        {
         if(arr[i] % 2 == 0)
             printf("%d ", arr[i]);
-    }
+       }
     return 0;
 }
+
